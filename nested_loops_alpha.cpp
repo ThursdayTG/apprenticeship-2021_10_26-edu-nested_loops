a47ee91dbc15e7ba3671f8fc6c7673887c7ac737
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2021-12-02 23:20:22 +0000

    Add files via upload

---
+++ nested_loops_alpha.cpp
@@ -0,0 +1,65 @@
+// inclusion of libraries
+#include <iostream>
+#include <string>
+
+// namespace
+using namespace std;
+
+
+int main(){
+
+	/*
+	 * Eingabe von Länge und Breite
+	 */
+
+	/**/
+	int	 length = 0, width = 0;
+
+	cout	<< "Laenge: ";		cin	 >> length;
+	cout	<< "Breite: ";		cin	 >> width;
+	cout	<< "\n";
+	/**/
+
+	/**/
+	for (int iy = 1;
+		 iy <= length;
+		 iy++){
+
+		for (int ix = 1;
+			ix <= width;
+			ix++){
+			cout	<< "# ";
+		}
+
+		cout	<< "\n";
+	}
+	/**/
+
+	/**
+	cout	<<	"test ... \n";
+	/**/
+
+	/**/
+	cout	<<	"\n";
+	cin.ignore();						// use this block to pause progress in console, unpauses on pressing any button. cin.ignore(); may not always be necessary.
+	cin.get();
+	cout	<<	"\033[2J\033[1;1H";		// clears screen, string of special characters that will translate to a clear screen command.
+	/**/
+
+	return 0;
+
+}
+
+/*
+	compile:
+g++ nested_loops_alpha.cpp -o nested_loops_alpha
+
+	run from console:
+./nested_loops_alpha
+
+	clear, compile, run:
+clear && g++ nested_loops_alpha.cpp -o nested_loops_alpha && ./nested_loops_alpha
+
+	name meaning:
+C++ for Schmid
+*/
